dc154792d9cdfd11b784d2cc59e1acc31b30b25f
szh-dell-920 <ziheng_song@126.com>
2017-12-27 11:19:41 +0800

    ss

--- exercise/leetcode/python_src/Leet686.py
+++ exercise/leetcode/python_src/Leet686.py
@@ -8,7 +8,7 @@ class Solution:
         if len(A) > len(B):
             if B in A:
                 return 1
-            elif B in A + A:
+            elif B in A * 2:
                 return 2
             else:
                 return -1
@@ -16,3 +16,11 @@ class Solution:
             if len(B) % len(A) == 0:
                 if B in A * (len(B) // len(A)):
                     return len(B) // len(A)
+                elif B in A * ((len(B) // len(A)) + 1):
+                    return (len(B) // len(A)) + 1
+            else:
+                if B in A * ((len(B) // len(A)) + 1):
+                    return len(B) // len(A)
+                elif B in A * ((len(B) // len(A)) + 2):
+                    return (len(B) // len(A)) + 2
+            return -1
