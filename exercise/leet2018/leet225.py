b92098fa32bf754c32f44faebba67df9ef08bbf8
laolao <ziheng_song@126.com>
2018-02-15 21:18:43 +0800

    ss

--- exercise/leet2018/leet225.py
+++ exercise/leet2018/leet225.py
@@ -1,8 +1,12 @@
+from collections import deque
 class MyStack:
     def __init__(self):
         """
         Initialize your data structure here.
         """
+        self.queue1 = deque()
+        self.queue2 = deque()
+        self.storeFlag = True
 
     def push(self, x):
         """
@@ -10,12 +14,19 @@ class MyStack:
         :type x: int
         :rtype: void
         """
+        if self.storeFlag:
+            self.queue1.append(x)
+        else:
+            self.queue2.append(x)
 
     def pop(self):
         """
         Removes the element on top of the stack and returns that element.
         :rtype: int
         """
+        
+        while len(len(self.queue1))
+
 
     def top(self):
         """
@@ -23,12 +34,13 @@ class MyStack:
         :rtype: int
         """
 
+
     def empty(self):
         """
         Returns whether the stack is empty.
         :rtype: bool
         """
-
+        return len(self.queue2) == 0 and len(self.queue1) == 0
 
 
         # Your MyStack object will be instantiated and called as such:
